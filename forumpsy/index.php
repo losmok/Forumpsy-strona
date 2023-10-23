942ccb5a015622fba79632e60ac94386ec72b98c
losmok <113143965+losmok@users.noreply.github.com>
2023-10-23 15:18:39 +0200

    Add files via upload

---
+++ forumpsy/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl.css">
+    <title>Forum o psach</title>
+</head>
+<body>
+    <div class="baner">
+        <h1>Forum miłośników psów</h1>
+    </div>
+    <div class="left">
+        <img src="avatar.jpg" alt="Użytkownik forum">
+        <!-- SCRIPT          -->
+        <video controls>
+            <source src="video.mp4" type="video/mp4">
+        </video>
+    </div>
+    <div class="right">
+        <form action="" method="POST">
+            <textarea id="komentarz" rows="4" cols="40"></textarea>
+            <button id="submit" name="submit">Dodaj odpowiedź</button>
+        </form>
+        <!-- SCRIPT2 -->
+        <h2>Odpowiedzi na pytanie</h2>
+        <ol>
+            <!-- SCRIPT3 -->
+        </ol>
+    </div>
+    <div class="footer">
+        <p>Autor Paweł lewandowski</p>
+        <a href="http://mojestrony.pl/" target="_blank">Zobacz nasze realizacje</a>
+    </div>
+</body>
+</html>
